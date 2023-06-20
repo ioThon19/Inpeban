734d50b501e365f1375302529d7632c69a9c6ab6
Lidya Gabriella _141 <86070778+Biella20@users.noreply.github.com>
2023-06-20 20:18:34 +0700

    Update beranda.blade.php

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -17,6 +17,7 @@ $data2 = json_decode($konten2, true);
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="dicoding:email" content="aanthonio19@gmail.com">
   <title>Inpeban</title>
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
